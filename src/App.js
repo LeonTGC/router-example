096b46099efa4bdeceffdc05017b0eb4444ca563
Cooper <leon.cooper@gceducationandskills.ac.uk>
2023-06-16 12:06:04 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+import Home from "./pages/Home"
+import About from "./pages/About"
+import Contact from "./pages/Contact"
+import Navbar from "./components/Navbar"
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <BrowserRouter>
+        <Navbar />
+        <h1>welcome to my sight</h1>
+        <Routes>
+          <Route 
+            path="/"
+            element={<Home />}  
+          />
+          <Route 
+            path="/about"
+            element={<About />}
+          />
+          <Route 
+            path='/contactme'
+            element={<Contact />}
+          />
+        </Routes>
+      </BrowserRouter>
     </div>
-  );
+  )
 }
-
-export default App;
+export default App
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,21 @@
+import { Link } from "react-router-dom"
+
+const Navbar = () => {
+    return (
+        <header>
+            <div>
+                <Link to="/">
+                    <h2>home</h2>
+                </Link>
+                <Link to="/about">
+                    <h2>about</h2>
+                </Link>
+                <Link to="/contactme">
+                    <h2>contact me</h2>
+                </Link>
+            </div>
+        </header>
+    )
+}
+
+export default Navbar
