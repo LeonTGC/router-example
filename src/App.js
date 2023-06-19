92ca398a2931befd80beacd1925f8fb190e98629
Cooper <leon.cooper@gceducationandskills.ac.uk>
2023-06-19 13:42:21 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,30 +1,7 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom"
-import Home from "./pages/Home"
-import About from "./pages/About"
-import Contact from "./pages/Contact"
-import Navbar from "./components/Navbar"
-
 const App = () => {
   return (
     <div>
-      <BrowserRouter>
-        <Navbar />
         <h1>welcome to my sight</h1>
-        <Routes>
-          <Route 
-            path="/"
-            element={<Home />}  
-          />
-          <Route 
-            path="/about"
-            element={<About />}
-          />
-          <Route 
-            path='/contactme'
-            element={<Contact />}
-          />
-        </Routes>
-      </BrowserRouter>
     </div>
   )
 }
